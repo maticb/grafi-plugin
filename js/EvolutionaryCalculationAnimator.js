edef2a553b585af86ef4509148e21d0cdb0f6f01
maticb <matic.blagus@hotmail.com>
2017-02-15 14:31:26 +0100

    Added input strign parsing functions

--- js/EvolutionaryCalculationAnimator.js
+++ js/EvolutionaryCalculationAnimator.js
@@ -17,7 +17,7 @@ Več grafov;
 * Jquery plugin for animating data of evolutionary calculation algorithms
 * Prerequisites:
 * - Jquery ^3.1.0
-* - ** RAFPolyfill.js (request animation frame polyfill, if using older browsers)
+* - ** RAFPolyfill.js (request animation frame polyfill, if using older browsers) https://gist.github.com/paulirish/1579671
 *
 * Init properties (OBJECT) containing:
 * - source 		string	REQUIRED	URL of the source file, or raw source data, depending on the settings (read below)
@@ -41,6 +41,193 @@ Več grafov;
 */
 
 
-$.fn.evoAnimate = function() {
-	this.html('test');
+$.fn.evoAnimate = function(props) {
+	// Static plugin private vars
+	var ARGS_NUM = 7; // Number of arguments in the first line of the input (this should never change, unless the format of the input string will change)
+	// Default values
+	var DEFAULT_CANVAS_SETTING = {
+		id: -1,
+		canvas: undefined,
+		ctx: undefined,
+		width: 300,
+		height: 300,
+	};
+
+	// Non-static private vars
+	var ANIMATION_DATA = {}; // Parsed animation data
+	var CANVAS_ARR = []; // Array of canvases
+
+	// Playback
+	var REQUEST_LOOP = undefined; // Request loop
+	var IS_LOADED = false; // Boolean that indicates if any data is loaded (so we can start playback)
+	var IS_PLAYING = false; // Indicates if animation is playing
+	var IS_SETUP = false; // Indicates if canvas and other elements needed have been setup
+	var PLAY_GEN = 1; // Current generation number
+	var PLAY_GEN_STEP = 1; // Step number within current generation
+
+
+	/*
+	* Parses entire input text (document)
+	* @param string	input 	String input
+	*/
+	function parseInput(input){
+		var rtrn = {};
+		rtrn.steps = []; // Algorithm steps will be stored in an array
+		// Parsing arguments
+		evolutionUtil.indexOfAll(input, ';', function(index, prev, count){
+			prev = prev > 0 ? prev + 1 : prev; // If previous index is above 0, add 1 (because that index is the ";")
+			var item =  this.substring(prev, index);
+			if('' === item) {
+				return false;
+			}
+			if(count < ARGS_NUM) {
+				parseArgs(rtrn, item, count);
+			} else {
+				// End loop
+				return true;
+			}
+			//console.log(index + ' ' + prev);
+		});
+		//Parse the remaining lines
+		evolutionUtil.indexOfAll(input, ['{','}'], function(index, prev, count){
+			if(0 === prev) //If we found the first "{" pass
+				return false;
+			prev += 1; // Add 1  to prev (because that index is the "{" or "}")
+			var item =  this.substring(prev, index);
+			if('' === item) {
+				return false;
+			}
+			parseLine(rtrn, item);
+		});
+		// Put steps of same generations in combined arrays, to save compute time on rendering!
+		reOrderStepsIntoGenerations(rtrn);
+		// If new data is loaded, canvases must be re-set up
+		IS_SETUP = false;
+		// Data is loaded
+		IS_LOADED  = true;
+		// Reset steps if new data is loaded
+		PLAY_GEN = 1;
+		PLAY_GEN_STEP = 0;
+		return rtrn;
+	}
+
+	/*
+	* Parses global arguments:
+	* algID; algName;[algParams];problemID; problemName;problemDim;[problemParams]
+	* @param object 	obj 	Object that stores the parsed values
+	* @param string		arg 	(one) argument value
+	* @param integer	argNum 	argument's number
+	*/
+	function parseArgs(obj, arg, argNum) {
+		switch(argNum){
+			case 0: { // algID
+				obj.algId = parseInt(arg);
+				break;
+			}
+			case 1: { // algName
+				obj.algName =  arg;
+				break;
+			}
+			case 2: { // [algParams]
+				obj.algParams = evolutionUtil.parseArray(arg);
+				break;
+			}
+			case 3: { // problemID
+				obj.problemId = parseInt(arg);
+				break;
+			}
+			case 4: { // problemName
+				obj.problemName =  arg;
+				break;
+			}
+			case 5: { // problemDim
+				obj.problemDim = parseInt(arg);
+				break;
+			}
+			case 6: { // [problemParams]
+				obj.problemParams = evolutionUtil.parseArray(arg);
+				break;
+			}
+		}
+		return obj;
+	}
+
+	/*
+	* Parses argument via it's number inside line
+	* @param string/array 	arg 	argument value
+	* @param integer 		argNum 	argument's number
+	*/
+	function parseLineArg(obj, arg, argNum) {
+		switch(argNum) {
+			case 0: { // id
+				obj.id = parseInt(arg);
+				break;
+			}
+			case 1: { // generation
+				obj.generation = parseInt(arg);
+				break;
+			}
+			case 2: { // [parentIds]
+				obj.parentIds = evolutionUtil.parseArray(arg);
+				break;
+			}
+			case 3: { // timestamp
+				obj.timestamp = arg;
+				break;
+			}
+			case 4: { // eval
+				// TODO: poglej kaj je eval; integer/float?
+				obj.eval = arg;
+				break;
+			}
+			case 5: { // fitness
+				obj.fitness = parseFloat(arg);
+				break;
+			}
+			case 6: { // [x]
+				obj.x = evolutionUtil.parseArray(arg);
+				break;
+			}
+
+		}
+		return obj;
+	}
+
+	/*
+	* Parses all the other lines of input
+	* {id; generation; [parentids]; timestamp; eval; fitness; [x]}
+	* @param object 	obj 	Object that stores the parsed values
+	* @param string 	line 	String of one line of the input
+	*/
+	function parseLine(obj, line) {
+		var lineObj = {};
+		// Check if last char in line is ";", if not add it (to make sure the loop works)
+		if(line[line.length -1] !== ';'){
+			line += ';';
+		}
+		evolutionUtil.indexOfAll(line, ';', function(index, prev, count){
+			prev = prev > 0 ? prev + 1 : prev; // If previous index is above 0, add 1 (because that index is the ";")
+			var item =  this.substring(prev, index);
+			if('' === item) {
+				return false;
+			}
+			parseLineArg(lineObj, item, count);
+		});
+		obj.steps.push(lineObj);
+	}
+
+
+	/*
+	* Function that checks the given properties and initializes the plugin
+	*/
+	function initialize() {
+		if(!props.hasOwnProperty('source')) {
+			// TODO: friendlier error messages
+			alert('Erorr: Source must be defined!');
+			return false;
+		}
+		return true;
+	}
+	// 	Initialize the plugin
+	return initialize() ? this : false;
 };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+var exampleInput  = ''+
+'1;ime1;["param1","param2","param3"];1;problem1;3;["param1","param2"];'+
+'{1;1;[-1,-1];00000;1;5;[3,3,3]}'+
+'{2;1;[-1,-1];00000;1;3;[2,3,1]}'+
+'{3;1;[-1,-1];00000;1;4;[6,1,5]}'+
+'{4;1;[-1,-1];00000;1;2;[3,1,1]}'+
+'{5;2;[1,3];00001;1;5;[6,6,7]}'+
+'{6;2;[2,4];00001;1;3;[2,3,1]}';
+
 $(document).ready(function(){
-	$('#main').evoAnimate();
+	$('#main').evoAnimate({
+		source: exampleInput,
+		sourceType: 'STRING',
+		display: [1,2]
+	});
 });
